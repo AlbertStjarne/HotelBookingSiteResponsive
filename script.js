cbff7b4dd1e6d406ebc40b08fa68733343c1b0a5
Albert Stjärne <albert.stjarne@gmail.com>
2020-05-05 09:27:44 +0200

    adds global styling, adds styling to header and to navigation, adds script to toggle navigation

---
+++ script.js
@@ -0,0 +1,6 @@
+const header = document.querySelector('.header');
+const hamburgerMenu = document.querySelector('.hamburger-menu');
+
+hamburgerMenu.addEventListener('click', function () {
+  header.classList.toggle('menu-open');
+});
